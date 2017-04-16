2909d18cb699f674e3ffb1532136214efae5a7b6
Michael Toth <michaeltoth@michaels-imac.local>
2017-04-16 16:37:51 -0400

    Initial Commit

---
+++ Bingo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bingo
+//
+//  Created by Michael Toth on 4/16/17.
+//  Copyright © 2017 Michael Toth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
